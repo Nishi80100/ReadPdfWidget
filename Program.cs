e4bf497f2a44efc17fe7245a3ee067e8e1c74a87
Nishi <Nishi@PF1KYEZ4211123>
2022-02-26 15:43:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Spire.Pdf;
+using Spire.Pdf.Widget;
+using Spire.Pdf.Fields;
+
+namespace ReadPdfWidget
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"C:\Users\nishi\OneDrive - Huron Consulting Group\Desktop\P_Folder\Projects\EditPDF\Docs\OoPdfFormExample.pdf");
+            PdfFormWidget formWidget = doc.Form as PdfFormWidget;
+            for (int i = 0; i < formWidget.FieldsWidget.List.Count; i++)
+            {
+                PdfField field = formWidget.FieldsWidget.List[i] as PdfField;
+                string fieldName = field.Name;
+                Console.WriteLine(fieldName);
+            }
+            Console.Read();
+
+        }
+    }
+}
